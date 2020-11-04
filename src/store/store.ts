dd24f7fd37f07b5f8b5fe5ea0d627c375b876f98
Matthew Reetz <matt@headway.io>
2020-11-04 15:03:13 -0600

    Add support for reactothon.

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,7 +3,6 @@ import { configureStore, Action } from '@reduxjs/toolkit'
 import { ThunkAction } from 'redux-thunk'
 import { useDispatch, useStore } from 'react-redux'
 import rootReducer, { RootState } from './rootReducer'
-<<<<<<< HEAD
 import Reactotron from '../../ReactotronConfig'
 
 const enhancers = []
@@ -14,11 +13,6 @@ if (Reactotron.createEnhancer) {
 const store = configureStore({
   reducer: rootReducer,
   enhancers,
-=======
-
-const store = configureStore({
-  reducer: rootReducer,
->>>>>>> Add redux toolkit. Fetch and display stats.
 })
 
 export type AppDispatch = typeof store.dispatch
