081e4d4d30bcd2674d1dc0439bf6c5fc2a05a068
Matthew Reetz <matt@headway.io>
2020-11-06 14:54:33 -0600

    Set status bar to light.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { ThemeProvider } from '@shopify/restyle'
+import { Platform, StatusBar } from 'react-native'
 import { theme } from './theme/theme'
 import NavigationRoot from './navigation/NavigationRoot'
 
@@ -9,6 +10,11 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaProvider>
+        {/* Will need to adapt status bar for light/dark modes */}
+        {Platform.OS === 'ios' && <StatusBar barStyle="light-content" />}
+        {Platform.OS === 'android' && (
+          <StatusBar translucent backgroundColor="transparent" />
+        )}
         <NavigationRoot />
       </SafeAreaProvider>
     </ThemeProvider>
