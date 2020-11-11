9627a27f5482fa9329d9aed199a2e98738bc442b
Andrew Allen <allenan@helium.com>
2020-11-11 15:32:27 -0800

    switch to latest version of expo-secure-store

--- src/utils/account.ts
+++ src/utils/account.ts
@@ -1,5 +1,5 @@
 import { Keypair, Mnemonic, Address } from '@helium/crypto-react-native'
-import * as SecureStore from './SecureStore'
+import * as SecureStore from 'expo-secure-store'
 
 export const createKeypair = async (givenMnemonic: Mnemonic | null = null) => {
   const mnemonic = givenMnemonic || (await Mnemonic.create())
@@ -11,21 +11,27 @@ export const createKeypair = async (givenMnemonic: Mnemonic | null = null) => {
   ])
 }
 
-export const getAddress = async (): Promise<Address> => {
+export const getAddress = async (): Promise<Address | undefined> => {
   const addressB58 = await SecureStore.getItemAsync('address')
-  return Address.fromB58(addressB58)
+  if (addressB58) {
+    return Address.fromB58(addressB58)
+  }
 }
 
-export const getMnemonic = async (): Promise<Mnemonic> => {
+export const getMnemonic = async (): Promise<Mnemonic | undefined> => {
   const wordsStr = await SecureStore.getItemAsync('mnemonic')
-  const words = JSON.parse(wordsStr)
-  return new Mnemonic(words)
+  if (wordsStr) {
+    const words = JSON.parse(wordsStr)
+    return new Mnemonic(words)
+  }
 }
 
-export const getKeypair = async (): Promise<Keypair> => {
+export const getKeypair = async (): Promise<Keypair | undefined> => {
   const keypairStr = await SecureStore.getItemAsync('keypair')
-  const keypairRaw = JSON.parse(keypairStr)
-  return new Keypair(keypairRaw)
+  if (keypairStr) {
+    const keypairRaw = JSON.parse(keypairStr)
+    return new Keypair(keypairRaw)
+  }
 }
 
 export const signOut = async () => {
