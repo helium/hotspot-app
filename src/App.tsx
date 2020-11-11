412721e47513881f206b9f118e7d364f5cfe413c
Andrew Allen <allenan@helium.com>
2020-11-11 15:48:41 -0800

    move global buffer definition into crypto-react-native

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,6 @@ import { Platform, StatusBar } from 'react-native'
 import { theme } from './theme/theme'
 import NavigationRoot from './navigation/NavigationRoot'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires, import/no-extraneous-dependencies
-global.Buffer = require('safe-buffer').Buffer
-
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
