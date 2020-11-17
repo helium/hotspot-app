189da6f29b90b1ffc24b1e16e1965f209dff5229
Matthew Reetz <matt@headway.io>
2020-11-17 16:04:03 -0600

    Remove redux persist.

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,16 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit'
-import { persistReducer } from 'redux-persist'
-import AsyncStorage from '@react-native-community/async-storage'
 import statsSlice from './stats/statsSlice'
 import userSlice from './user/userSlice'
 
-const userPersistConfig = {
-  key: 'user',
-  storage: AsyncStorage,
-}
-
 const rootReducer = combineReducers({
-  user: persistReducer(userPersistConfig, userSlice.reducer),
+  user: userSlice.reducer,
   stats: statsSlice.reducer,
 })
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,6 @@
-import { configureStore, Action, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { configureStore, Action } from '@reduxjs/toolkit'
 import { ThunkAction } from 'redux-thunk'
 import { useDispatch, useStore } from 'react-redux'
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist'
-import AsyncStorage from '@react-native-community/async-storage'
 import rootReducer, { RootState } from './rootReducer'
 import Reactotron from '../../ReactotronConfig'
 
@@ -20,26 +9,10 @@ if (Reactotron.createEnhancer) {
   enhancers.push(Reactotron.createEnhancer())
 }
 
-const persistConfig = {
-  key: 'root',
-  version: 1,
-  storage: AsyncStorage,
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware({
-    immutableCheck: false,
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
+  reducer: rootReducer,
   enhancers,
 })
-
-export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type AppThunk = ThunkAction<void, RootState, null, Action<string>>
