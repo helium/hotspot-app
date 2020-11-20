d435e2c7f0f00b48ef20dda89ead9c2ef74ef2c0
Matthew Reetz <matt@headway.io>
2020-11-19 21:49:59 -0600

    Update enter passphrase screen layout to better fit small devices.

--- src/features/onboarding/accountEnterPassphrase/AccountEnterPassphraseScreen.tsx
+++ src/features/onboarding/accountEnterPassphrase/AccountEnterPassphraseScreen.tsx
@@ -10,6 +10,7 @@ import PhraseChip from './PhraseChip'
 import Button from '../../../components/Button'
 import { OnboardingNavigationProp } from '../onboardingTypes'
 import { getMnemonic } from '../../../utils/account'
+import SafeAreaBox from '../../../components/SafeAreaBox'
 
 const testIndices = __DEV__
   ? [0, 1, 2]
@@ -85,52 +86,45 @@ const AccountEnterPassphraseScreen = () => {
 
   return (
     <>
-      <Box
+      <SafeAreaBox
         backgroundColor="mainBackground"
         flex={1}
         alignItems="center"
-        flexDirection="column-reverse"
         padding="l"
       >
-        <Box>
-          <Box flexDirection="row" flexWrap="wrap">
-            {challengeWords.map((word) => (
-              <PhraseChip
-                marginRight="s"
-                marginBottom="s"
-                key={word}
-                title={word}
-                onPress={() => !correctWord && onPressWord(word)}
-              />
-            ))}
-          </Box>
-          <Button
-            title={t('account_setup.confirm.forgot')}
-            marginTop="m"
-            onPress={navigation.goBack}
-          />
-        </Box>
-        <Box
-          flex={1}
-          alignItems="center"
-          justifyContent="center"
-          alignContent="center"
-        >
-          <Box height={162} justifyContent="center" alignItems="center">
-            <Text variant="header" numberOfLines={1} adjustsFontSizeToFit>
-              {t('account_setup.confirm.title')}
-            </Text>
-            <Text variant="body">
-              {t('account_setup.confirm.subtitle', {
-                ordinal: t(`ordinals.${testIndices[step]}`),
-              })}
-            </Text>
+        <Box flex={1} justifyContent="center" alignItems="center">
+          <Text variant="header" numberOfLines={1} adjustsFontSizeToFit>
+            {t('account_setup.confirm.title')}
+          </Text>
+          <Text variant="body">
+            {t('account_setup.confirm.subtitle', {
+              ordinal: t(`ordinals.${testIndices[step]}`),
+            })}
+          </Text>
+          <Box height={100}>
             {correctWord && (
-              <PhraseChip marginTop="xxl" selected title={correctWord} />
+              <PhraseChip marginTop="xl" selected title={correctWord} />
             )}
           </Box>
         </Box>
-      </Box>
+        <Box flex={1} flexDirection="row" flexWrap="wrap">
+          {challengeWords.map((word) => (
+            <PhraseChip
+              marginRight="s"
+              marginBottom="s"
+              key={word}
+              title={word}
+              onPress={() => !correctWord && onPressWord(word)}
+            />
+          ))}
+        </Box>
+        <Button
+          title={t('account_setup.confirm.forgot')}
+          marginTop="m"
+          onPress={navigation.goBack}
+        />
+        <Box />
+      </SafeAreaBox>
       <Modal
         presentationStyle="overFullScreen"
         transparent
