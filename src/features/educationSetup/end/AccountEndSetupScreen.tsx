73279160d39fc86b8dd8147992f47d53e8bc9197
Matthew Reetz <matt@headway.io>
2020-11-24 08:30:25 -0600

    Fix AccountEndSetup layout on small screens.

--- src/features/educationSetup/end/AccountEndSetupScreen.tsx
+++ src/features/educationSetup/end/AccountEndSetupScreen.tsx
@@ -19,13 +19,16 @@ const AccountEndSetupScreen = () => {
       alignItems="center"
       paddingHorizontal="l"
     >
-      <HotspotFront />
-
+      <Box height={{ phone: 300, smallPhone: 200 }}>
+        <HotspotFront height="100%" />
+      </Box>
       <Text variant="header">{t('hotspot_setup.start.title')}</Text>
       <Text variant="body" textAlign="center" marginVertical="m">
         {t('hotspot_setup.start.subtitle_1')}
       </Text>
-      <Text variant="body">{t('hotspot_setup.start.subtitle_2')}</Text>
+      <Text textAlign="center" width={260} variant="body">
+        {t('hotspot_setup.start.subtitle_2')}
+      </Text>
 
       <Box width="100%" flex={1} justifyContent="flex-end">
         <Button
