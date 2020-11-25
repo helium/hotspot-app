58c1960968e890d94e45209f90a085de83e45e8b
Matthew Reetz <matt@headway.io>
2020-11-25 07:51:56 -0600

    Add mock for react-native-device-info

--- jestSetup.js
+++ jestSetup.js
@@ -1,7 +1,9 @@
 import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock'
+import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock'
 
 global.window = {}
 global.window = global
 
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper')
 jest.mock('@react-native-community/async-storage', () => mockAsyncStorage)
+jest.mock('react-native-device-info', () => mockRNDeviceInfo)
