0cbea36aa24a496cb2a8e715f9d68032fb4734a7
Matthew Reetz <matt@headway.io>
2020-12-07 14:44:21 -0600

    Set underlying nav bar color on android.

--- src/navigation/NavigationRoot.tsx
+++ src/navigation/NavigationRoot.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { useSelector } from 'react-redux'
+import changeNavigationBarColor from 'react-native-navigation-bar-color'
 import Onboarding from '../features/onboarding/OnboardingNavigator'
 import Education from '../features/educationSetup/EducationNavigator'
 import SplashScreen from '../features/splash/SplashScreen'
@@ -10,6 +11,7 @@ import { useAppDispatch } from '../store/store'
 import { RootState } from '../store/rootReducer'
 import defaultScreenOptions from './defaultScreenOptions'
 import RootNav from './main/HomeNavigator'
+import { useColors } from '../theme/themeHooks'
 
 const RootStack = createStackNavigator()
 
@@ -19,6 +21,11 @@ const NavigationRoot = () => {
     (state: RootState) => state.user,
   )
   const dispatch = useAppDispatch()
+  const colors = useColors()
+
+  useEffect(() => {
+    changeNavigationBarColor(colors.primaryBackground, true, false)
+  }, [colors.primaryBackground])
 
   useEffect(() => {
     dispatch(restoreUser())
