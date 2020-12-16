e711175575b400b0d4d6c0e1c6e0b693bd2bedb7
Andrew Allen <allenan@helium.com>
2020-12-16 14:36:59 -0800

    use ios 13 modal presentation

--- ios/Helium/AppDelegate.m
+++ ios/Helium/AppDelegate.m
@@ -48,7 +48,7 @@ static void InitializeFlipper(UIApplication *application) {
                                                    moduleName:@"Helium"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.0f green:0.0f blue:0.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
