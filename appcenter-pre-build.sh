330dfebe041c3f26b660a57304b637a63bc55a14
Tyler Whitman <twhit093@gmail.com>
2021-01-08 15:14:09 -0800

    remove env logging

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 
 # add required app center environment variables into a .env file for use with react-native-config
-echo ONE_SIGNAL_APP_ID=$ONE_SIGNAL_APP_ID
 echo ONE_SIGNAL_APP_ID=$ONE_SIGNAL_APP_ID >> .env
-
-echo MAPBOX_ACCESS_TOKEN=$MAPBOX_ACCESS_TOKEN
 echo MAPBOX_ACCESS_TOKEN=$MAPBOX_ACCESS_TOKEN >> .env
-
-echo STAKING_API_BASE_URL=$STAKING_API_BASE_URL
 echo STAKING_API_BASE_URL=$STAKING_API_BASE_URL >> .env
-
-echo WALLET_API_BASE_URL=$WALLET_API_BASE_URL
 echo WALLET_API_BASE_URL=$WALLET_API_BASE_URL >> .env
-
-echo STAKING_TOKEN=$STAKING_TOKEN
 echo STAKING_TOKEN=$STAKING_TOKEN >> .env
-
-echo SENTRY_DSN=$SENTRY_DSN
 echo SENTRY_DSN=$SENTRY_DSN >> .env
