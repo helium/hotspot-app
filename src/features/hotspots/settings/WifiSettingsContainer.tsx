acb86fe25185d89240a4b21c9a94218e3ce2948b
Matt Reetz <matt@helium.com>
2021-01-30 08:49:38 -0600

    Handle back for wifi flow.

--- src/features/hotspots/settings/WifiSettingsContainer.tsx
+++ src/features/hotspots/settings/WifiSettingsContainer.tsx
@@ -1,18 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import animateTransition from '../../../utils/animateTransition'
 import WifiSettings from './WifiSettings'
 import WifiSetup from './WifiSetup'
+import { useHotspotSettingsContext } from './HotspotSettingsProvider'
 
 type Props = { onFinished: () => void }
 const WifiSettingsContainer = ({ onFinished }: Props) => {
-  const [selectedNetwork, setSelectedNetwork] = useState<string | undefined>(
-    undefined,
-  )
+  const [selectedNetwork, setSelectedNetwork] = useState<string | null>(null)
+
+  const { enableBack } = useHotspotSettingsContext()
 
   const handleNetworkSelected = (wifi: string) => {
     setSelectedNetwork(wifi)
     animateTransition()
   }
+
+  const handleBack = useCallback(() => {
+    animateTransition()
+    if (selectedNetwork) {
+      setSelectedNetwork(null)
+    } else {
+      onFinished()
+    }
+  }, [onFinished, selectedNetwork])
+
+  useEffect(() => {
+    enableBack(handleBack)
+  }, [enableBack, handleBack])
 
   if (selectedNetwork) {
     return <WifiSetup network={selectedNetwork} onFinished={onFinished} />
