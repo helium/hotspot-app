f7925be4deaf3acb2b5f80737aae68eae77de3e3
Matt Reetz <matt@helium.com>
2021-02-03 14:54:09 -0600

    Handle splash screen on Android.

--- android/app/src/main/java/com/helium/wallet/generated/BasePackageList.java
+++ android/app/src/main/java/com/helium/wallet/generated/BasePackageList.java
@@ -15,7 +15,8 @@ public class BasePackageList {
         new expo.modules.localauthentication.LocalAuthenticationPackage(),
         new expo.modules.location.LocationPackage(),
         new expo.modules.permissions.PermissionsPackage(),
-        new expo.modules.securestore.SecureStorePackage()
+        new expo.modules.securestore.SecureStorePackage(),
+        new expo.modules.splashscreen.SplashScreenPackage()
     );
   }
 }
