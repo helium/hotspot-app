a67ff04fcce54b1cfa5fca9367512f9550c76889
Matt Reetz <matt@helium.com>
2021-02-03 12:56:52 -0600

    Show splash screen until initial data is loaded.

--- src/navigation/NavigationRoot.tsx
+++ src/navigation/NavigationRoot.tsx
@@ -1,46 +1,28 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { useSelector } from 'react-redux'
 import changeNavigationBarColor from 'react-native-navigation-bar-color'
 import Onboarding from '../features/onboarding/OnboardingNavigator'
 import Education from '../features/educationSetup/EducationNavigator'
-import SplashScreen from '../features/splash/SplashScreen'
-import { restoreUser } from '../store/user/appSlice'
-import { useAppDispatch } from '../store/store'
 import { RootState } from '../store/rootReducer'
 import defaultScreenOptions from './defaultScreenOptions'
 import RootNav from './main/HomeNavigator'
 import { useColors } from '../theme/themeHooks'
-import { fetchInitialData } from '../store/helium/heliumDataSlice'
 
 const RootStack = createStackNavigator()
 
 const NavigationRoot = () => {
-  const [showSplash, setShowSplash] = useState(true)
-  const { isBackedUp, isEducated, isRestored } = useSelector(
+  const { isBackedUp, isEducated } = useSelector(
     (state: RootState) => state.app,
   )
-  const dispatch = useAppDispatch()
   const colors = useColors()
 
   useEffect(() => {
     changeNavigationBarColor(colors.primaryBackground, true, false)
   }, [colors.primaryBackground])
 
-  useEffect(() => {
-    dispatch(restoreUser())
-    dispatch(fetchInitialData())
-
-    setTimeout(() => {
-      setShowSplash(false)
-    }, 1250)
-  }, [dispatch])
-
   const currentScreen = useCallback(() => {
-    if (showSplash || !isRestored)
-      return <RootStack.Screen name="Splash" component={SplashScreen} />
-
     if (!isBackedUp)
       return (
         <RootStack.Screen
@@ -54,7 +36,7 @@ const NavigationRoot = () => {
       return <RootStack.Screen name="Education" component={Education} />
 
     return <RootStack.Screen name="MainTab" component={RootNav} />
-  }, [showSplash, isEducated, isBackedUp, isRestored])
+  }, [isEducated, isBackedUp])
 
   return (
     <NavigationContainer>
