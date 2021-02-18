2c4e445818eb6426ff184c0e9b40a35fdb66f0f1
Matt Reetz <matt@helium.com>
2021-02-18 14:58:37 -0600

    Fix activity bottom sheet vertical alignment.

--- src/features/wallet/root/walletLayout.tsx
+++ src/features/wallet/root/walletLayout.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 import React from 'react'
 import { SafeAreaInsetsContext } from 'react-native-safe-area-context'
 import { wh, hp } from '../../../utils/layout'
 
 export const withWalletLayout = (WrappedComponent: any) => (props: any) => {
+  const tabBarHeight = useBottomTabBarHeight()
+
   return (
     <SafeAreaInsetsContext.Consumer>
       {(insets) => {
@@ -13,32 +16,30 @@ export const withWalletLayout = (WrappedComponent: any) => (props: any) => {
           chartHeight: hp(25),
           balanceHeight: hp(18),
           balanceInnerTranslate: hp(1),
-          altCurrencyHeight: 60,
-          navbarHeight: 49,
-          bottomHeight: insets?.bottom || 0,
-          cardHandleHeight: 14,
+          altCurrencyHeight: 49, // TODO: Use this value in balance card
+          navbarHeight: tabBarHeight,
+          bottomHeight: insets?.bottom || 1,
+          cardHandleHeight: 48,
         }
+        const belowStatusBar =
+          wh -
+          (walletLayout.navbarHeight +
+            walletLayout.cardHandleHeight +
+            walletLayout.notchHeight -
+            walletLayout.bottomHeight)
 
         const walletAnimationPoints = {
           dragMax:
-            wh -
-            walletLayout.notchHeight -
-            40 -
-            walletLayout.navbarHeight -
+            belowStatusBar -
             walletLayout.headerHeight -
             walletLayout.balanceHeight +
-            walletLayout.altCurrencyHeight -
-            walletLayout.cardHandleHeight,
+            walletLayout.altCurrencyHeight,
           dragMid:
-            wh -
-            walletLayout.notchHeight -
-            walletLayout.bottomHeight -
-            walletLayout.navbarHeight -
+            belowStatusBar -
             walletLayout.headerHeight -
-            walletLayout.chartHeight -
             walletLayout.balanceHeight -
-            walletLayout.cardHandleHeight,
-          dragMin: insets?.bottom || 1,
+            walletLayout.chartHeight,
+          dragMin: walletLayout.bottomHeight,
         }
         return (
           <WrappedComponent
