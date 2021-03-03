4455c5f23bebaa0a81d8b2d71217f3b3ef147dec
Matt Reetz <matt@helium.com>
2021-03-03 14:44:01 -0600

    Fetch price data from coin gecko and store in helium data slice.

---
+++ src/utils/coinGeckoClient.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/prefer-default-export
+export const getCurrentPrices = async () => {
+  const response = await fetch(
+    'https://api.coingecko.com/api/v3/coins/helium?localization=false&tickers=false&community_data=false&developer_data=false',
+  )
+  const json = await response.json()
+  if (json?.market_data?.current_price) {
+    return json?.market_data?.current_price as Record<string, number>
+  }
+  return undefined
+}
