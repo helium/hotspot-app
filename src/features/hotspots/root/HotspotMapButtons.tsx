9982bd74e53fd236df6dcbeebd64b1b4bc79565e
Matt Reetz <matthewcarlreetz@gmail.com>
2021-03-04 13:05:35 -0600

    Add an indicator when loading hotspot details. (#344)
    Co-authored-by: Matt Reetz <matt@helium.com>
    Co-authored-by: Tyler Whitman <twhit093@gmail.com>

--- src/features/hotspots/root/HotspotMapButtons.tsx
+++ src/features/hotspots/root/HotspotMapButtons.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Animated, {
   Extrapolate,
   interpolate,
@@ -6,6 +6,7 @@ import Animated, {
 } from 'react-native-reanimated'
 import EyeCircleButton from '@assets/images/eye-circle-button.svg'
 import EyeCircleButtonYellow from '@assets/images/eye-circle-button-yellow.svg'
+import { ActivityIndicator } from 'react-native'
 import Box from '../../../components/Box'
 import TouchableOpacityBox from '../../../components/TouchableOpacityBox'
 
@@ -14,11 +15,13 @@ const HotspotMapButtons = ({
   showWitnesses,
   toggleShowWitnesses,
   isVisible = true,
+  loading,
 }: {
   animatedPosition: Animated.SharedValue<number>
   showWitnesses: boolean
   toggleShowWitnesses: () => void
   isVisible?: boolean
+  loading: boolean
 }) => {
   const style = useAnimatedStyle(
     () => ({
@@ -39,12 +42,37 @@ const HotspotMapButtons = ({
     [animatedPosition, isVisible],
   )
 
+  const loadingStyle = useMemo(
+    () => ({
+      backgroundColor: showWitnesses ? '#FCC945' : '#555A82',
+      borderRadius: 22,
+    }),
+    [showWitnesses],
+  )
+
   return (
     <Animated.View style={style}>
       <Box padding="m" flexDirection="row">
-        <TouchableOpacityBox onPress={toggleShowWitnesses}>
-          {showWitnesses ? <EyeCircleButtonYellow /> : <EyeCircleButton />}
-        </TouchableOpacityBox>
+        {!loading && (
+          <TouchableOpacityBox onPress={toggleShowWitnesses}>
+            {showWitnesses ? <EyeCircleButtonYellow /> : <EyeCircleButton />}
+          </TouchableOpacityBox>
+        )}
+        {loading && (
+          <Box
+            height={43}
+            width={43}
+            alignItems="center"
+            justifyContent="center"
+            style={loadingStyle}
+          >
+            <ActivityIndicator
+              size="small"
+              color={showWitnesses ? 'white' : 'black'}
+            />
+          </Box>
+        )}
+
         {/* TODO: hex grid button */}
         {/* <TouchableOpacityBox marginStart="s"> */}
         {/*  <HexCircleButton /> */}
