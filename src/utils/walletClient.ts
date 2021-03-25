fc774f62ceed5be7553c3eb01b894631ff8d0cf5
Andrew Allen <allenan@helium.com>
2021-03-25 09:56:53 -0700

    revert wallet api route

--- src/utils/walletClient.ts
+++ src/utils/walletClient.ts
@@ -12,8 +12,7 @@ const makeRequest = async (url: string, opts: RequestInit) => {
       throw new Error('no token')
     }
 
-    // const route = [Config.WALLET_API_BASE_URL, url].join('/')
-    const route = ['http://localhost:4000/api', url].join('/')
+    const route = [Config.WALLET_API_BASE_URL, url].join('/')
 
     const response = await fetch(route, {
       ...opts,
