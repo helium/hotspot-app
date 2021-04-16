e74979816fe592be9271a6b2ee2e534a8917fe0b
Tyler Whitman <twhit093@gmail.com>
2021-04-16 14:09:52 -0700

    fix crash on app launch

--- src/utils/polyfill.js
+++ src/utils/polyfill.js
@@ -1,7 +1,9 @@
 import { Platform } from 'react-native'
 
 // noinspection JSConstantReassignment
-global.document = {}
+global.document = {
+  addEventListener: () => {},
+}
 
 if (Platform.OS === 'android') {
   require('number-to-locale-string-polyfill')
