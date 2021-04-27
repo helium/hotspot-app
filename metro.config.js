82f10f5ad1183131e34e0b4e5cec2c4f373becce
Tyler Whitman <twhit093@gmail.com>
2021-04-27 16:36:49 -0700

    enable inline requires

--- metro.config.js
+++ metro.config.js
@@ -7,6 +7,12 @@ module.exports = (async () => {
   return {
     transformer: {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
