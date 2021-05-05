7d6f70c2d30ffad2e4b70c66dc8ad8b68bc7ceab
Tyler Whitman <twhit093@gmail.com>
2021-05-05 12:49:49 -0700

    fix action sheet scrolling on Android

--- src/components/HeliumActionSheet.tsx
+++ src/components/HeliumActionSheet.tsx
@@ -5,13 +5,14 @@ import Close from '@assets/images/close.svg'
 import CarotDown from '@assets/images/carot-down.svg'
 import Kabob from '@assets/images/kabob.svg'
 import { useTranslation } from 'react-i18next'
-import { FlatList, Modal, StyleSheet } from 'react-native'
+import { Modal, StyleSheet } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import {
   useAnimatedStyle,
   useSharedValue,
   withSpring,
 } from 'react-native-reanimated'
+import { FlatList } from 'react-native-gesture-handler'
 import { Colors, Theme } from '../theme/theme'
 import HeliumActionSheetItem, {
   HeliumActionSheetItemHeight,
@@ -152,19 +153,21 @@ const HeliumActionSheet = ({
 
   const footer = useMemo(() => {
     return (
-      <TouchableOpacityBox
-        onPress={handleClose}
-        style={styles.cancelContainer}
-        height={49}
-        marginVertical="m"
-        alignItems="center"
-        justifyContent="center"
-        borderRadius="ms"
-      >
-        <Text variant="medium" fontSize={18} style={styles.cancelText}>
-          {t('generic.cancel')}
-        </Text>
-      </TouchableOpacityBox>
+      <Box marginBottom="xl">
+        <TouchableOpacityBox
+          onPress={handleClose}
+          style={styles.cancelContainer}
+          height={49}
+          marginVertical="m"
+          alignItems="center"
+          justifyContent="center"
+          borderRadius="ms"
+        >
+          <Text variant="medium" fontSize={18} style={styles.cancelText}>
+            {t('generic.cancel')}
+          </Text>
+        </TouchableOpacityBox>
+      </Box>
     )
   }, [handleClose, t])
 
