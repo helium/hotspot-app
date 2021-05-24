a227c193a00351d7fa6e535d4b1b50d9d66ccc94
Matt Reetz <matthewcarlreetz@gmail.com>
2021-05-24 10:09:49 -0500

    Add additional logging/breadcrumbs for http requests

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,10 @@
 import * as Sentry from '@sentry/react-native'
 import Config from 'react-native-config'
 
+export const prettyPrintToConsole = (whatever: any, prefix = '') => {
+  console.log(`${prefix}\n${JSON.stringify(whatever, null, 2)}`)
+}
+
 export const init = () => {
   if (__DEV__) return
 
@@ -20,18 +24,18 @@ export const error = (e: unknown) => {
   Sentry.captureException(e)
 }
 
-export const breadcrumb = (
-  message: string,
-  data?: {
-    [key: string]: unknown
-  },
-) => {
-  Sentry.addBreadcrumb({
+export const breadcrumb = (message: string, data?: Sentry.Breadcrumb) => {
+  const crumb = {
     message,
     data,
-  })
-}
+  }
+  if (__DEV__ && Config.LOG_BREADCRUMBS === 'true') {
+    if (data) {
+      prettyPrintToConsole(crumb)
+    } else {
+      console.log(message)
+    }
+  }
 
-export const prettyPrintToConsole = (whatever: any, prefix = '') => {
-  console.log(`${prefix}\n${JSON.stringify(whatever, null, 2)}`)
+  Sentry.addBreadcrumb(crumb)
 }
