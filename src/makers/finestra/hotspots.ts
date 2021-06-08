3a282affc83ff1247ed32481dc83d467274e77d9
Matt Reetz <matthewcarlreetz@gmail.com>
2021-06-08 15:24:24 -0500

    Update ja translations.

--- src/makers/finestra/hotspots.ts
+++ src/makers/finestra/hotspots.ts
@@ -18,6 +18,18 @@ const FINESTRA_MINER = {
         'You can enable Bluetooth after 5 minutes by pressing the button on the side of the Finestra Miner',
       ],
     },
+    ja: {
+      diagnostic:
+        '<b><white>診断サポートにより、FinestraはHotspotの問題を安全な方法で特定できます。</white></b>\n\nFinestraが秘密キーにアクセスすることはありません。お使いのHotspotにのみアクセスし、ネットワーク上の他のデバイスにはアクセスしません。\n\n診断サポートをオプトアウトする場合は、Hotspotの購入時に使用したメールアドレスを使用して、<purple><b>support@thefinestra.com</b></purple>までメールでご連絡ください。',
+      power: [
+        'アンテナを取り付け、窓の近くにある付属の電源アダプターに差し込みます。',
+        'Finestra Minerの電源を入れると、青いLEDが点灯します。',
+      ],
+      bluetooth: [
+        'Finestra Minerの電源がオンになると、自動的にBluetoothが5分間有効になります。\n\nHotspotが完全に起動するまでに最大で1分かかる場合があります。',
+        'Finestra Minerの側面にあるボタンを押すと、5分後にBluetoothが有効になります',
+      ],
+    },
   },
   antenna: {
     default: ANTENNAS.FINESTRA_US,
--- src/makers/longAP/hotspots.ts
+++ src/makers/longAP/hotspots.ts
@@ -18,6 +18,18 @@ const LONGAP_ONE = {
         'Once the PWR LED is slowly blinking\n\nPress Next to scan.',
       ],
     },
+    ja: {
+      diagnostic:
+        '<b><white>診断サポートにより、LongAPはHotspotの問題を安全な方法で特定できます。</white></b>\n\nLongAPが秘密キーにアクセスすることはありません。お使いのHotspotにのみアクセスし、ネットワーク上の他のデバイスにはアクセスしません。\n\n診断サポートをオプトアウトする場合は、Hotspotの購入時に使用したメールアドレスを使用して、<purple><b>support@longap.com</b></purple>までメールでご連絡ください。',
+      power: [
+        'アンテナを取り付け、付属の電源アダプターに差し込みます。',
+        'LongAP Oneの電源を入れると、電源LEDが点灯します。',
+      ],
+      bluetooth: [
+        'ペーパークリップを使用して、LEDの右側の小さな穴にあるボタンを短く押します。',
+        '電源LEDがゆっくりと点滅します。\n\n「次へ」を押してスキャンします。',
+      ],
+    },
   },
   antenna: {
     default: ANTENNAS.LONG_AP_ONE_EU,
