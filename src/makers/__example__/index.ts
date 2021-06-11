c5c98bcdd9fa4f538e604ea979f366b12485b26d
Matt Reetz <matthewcarlreetz@gmail.com>
2021-06-11 09:02:32 -0500

    Create maker guide Move svgs to maker folders.

---
+++ src/makers/__example__/index.ts
@@ -0,0 +1,9 @@
+import antennas from './antennas'
+import hotspots from './hotspots'
+
+export default {
+  antennas,
+  hotspots,
+  id: 99999, // TODO: Get maker id from onboarding server
+  supportEmail: 'support@examplemaker.com',
+}
