1358beffdf55d42a64faf943f550183e7a3dcbf2
Matt Reetz <matthewcarlreetz@gmail.com>
2021-06-15 11:08:14 -0500

    Remove app.test

--- src/__tests__/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import 'react-native'
-import React from 'react'
-import App from '../App'
-import { render } from '../utils/testUtils'
-
-it('renders correctly', () => {
-  expect(render(<App />)).toBeDefined()
-})
