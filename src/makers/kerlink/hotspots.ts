5fb83cd245028b6ace49954c910c0c68d4adb2c2
Benoit Duffez <benoit.duffez@upactivity.com>
2021-06-23 14:35:20 -0700

    add kerlink
    this is based over https://github.com/helium/hotspot-app/pull/678

---
+++ src/makers/kerlink/hotspots.ts
@@ -0,0 +1,73 @@
+import HotspotIcon_iFemto from '@ifemto.svg'
+import HotspotIcon_iFemtoEvo from '@ifemto_evo.svg'
+import HotspotIcon_iStation from '@istation.svg'
+import { MakerHotspot } from '../hotspotMakerTypes'
+import ANTENNAS from './antennas'
+
+const qrKerlink = [
+  'Please visit the following URL from your computer or directly your phone by selecting the link below:',
+  'https://helium-onboarding.kerlink.com/?wallet=WALLET|helium-onboarding.kerlink.com',
+]
+
+const KERLINK_IFEMTO = {
+  name: 'Kerlink iFemtoCell',
+  onboardType: 'QR',
+  icon: HotspotIcon_iFemto,
+  translations: {
+    en: {
+      diagnostic:
+        '<b><white>Diagnostic support allows Kerlink to identify issues with your Hotspot in a secure way.</white></b>\n\nKerlink will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>support@helium.com</b></purple> from the email used to purchase the Hotspot.',
+      power: [
+        'Attach the antenna and plug in the provided power adapter.',
+        'Your Hotspot will boot up, and its light will become Green when ready.',
+      ],
+      qr: qrKerlink,
+    },
+  },
+  antenna: {
+    default: ANTENNAS.KERLINK_IFEMTO,
+  },
+} as MakerHotspot
+
+const KERLINK_IFEMTO_EVO = {
+  name: 'Kerlink iFemtoCell Evolution',
+  onboardType: 'QR',
+  icon: HotspotIcon_iFemtoEvo,
+  translations: {
+    en: {
+      diagnostic:
+        '<b><white>Diagnostic support allows Kerlink to identify issues with your Hotspot in a secure way.</white></b>\n\nKerlink will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>support@helium.com</b></purple> from the email used to purchase the Hotspot.',
+      power: [
+        'Attach the antenna and plug in the provided power adapter.',
+        'Your Hotspot will boot up, and its light will become Green when ready.',
+      ],
+      qr: qrKerlink,
+    },
+  },
+  antenna: {
+    default: ANTENNAS.KERLINK_IFEMTO_EVO,
+  },
+} as MakerHotspot
+
+const KERLINK_ISTATION = {
+  name: 'Kerlink iStation',
+  onboardType: 'QR',
+  icon: HotspotIcon_iStation,
+  translations: {
+    en: {
+      diagnostic:
+        '<b><white>Diagnostic support allows Kerlink to identify issues with your Hotspot in a secure way.</white></b>\n\nKerlink will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>support@helium.com</b></purple> from the email used to purchase the Hotspot.',
+      power: [
+        'Attach the antenna and plug in the provided power adapter.',
+        'Your Hotspot will boot up, and its light will become Green when ready.',
+      ],
+      qr: qrKerlink,
+    },
+  },
+  antenna: {
+    default: ANTENNAS.KERLINK_ISTATION_EU,
+    us: ANTENNAS.KERLINK_ISTATION_US,
+  },
+} as MakerHotspot
+
+export default { KERLINK_IFEMTO, KERLINK_IFEMTO_EVO, KERLINK_ISTATION }
