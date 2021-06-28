e237e4660cf093f7500174c05786d0e878c9e332
Matt Reetz <matthewcarlreetz@gmail.com>
2021-06-28 16:37:49 -0500

    Add animal name to stake and change color to purple.

--- src/features/wallet/root/ActivityDetails/StakeValidator.tsx
+++ src/features/wallet/root/ActivityDetails/StakeValidator.tsx
@@ -4,21 +4,33 @@ import {
   PendingTransaction,
   StakeValidatorV1,
 } from '@helium/http'
+import animalName from 'angry-purple-tiger'
+import Cloud from '@assets/images/stakeCloud.svg'
 import PaymentItem from './PaymentItem'
+import Box from '../../../../components/Box'
+import Text from '../../../../components/Text'
 
 type Props = { item: AnyTransaction | PendingTransaction }
 const StakeValidator = ({ item }: Props) => {
   if (item.type !== 'stake_validator_v1') return null
 
-  const rewards = item as StakeValidatorV1
+  const stakeValidator = item as StakeValidatorV1
   return (
-    <PaymentItem
-      text={rewards.address}
-      mode="from"
-      isMyAccount
-      isLast
-      isFirst
-    />
+    <>
+      <Box flexDirection="row" marginBottom="s">
+        <Cloud />
+        <Text variant="medium" fontSize={15} color="black" marginLeft="s">
+          {animalName(stakeValidator.address)}
+        </Text>
+      </Box>
+      <PaymentItem
+        text={stakeValidator.address}
+        mode="from"
+        isMyAccount
+        isLast
+        isFirst
+      />
+    </>
   )
 }
 
