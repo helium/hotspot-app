2c2b7a8ff746320601875191771e8e17bcdabdab
odenSu <308569762@qq.com>
2021-06-30 09:26:54 +0800

    change the default antenna gain

--- src/makers/sensecap/antennas.ts
+++ src/makers/sensecap/antennas.ts
@@ -2,12 +2,12 @@ import { MakerAntenna } from '../antennaMakerTypes'
 
 const SENSECAP_US = {
   name: 'SenseCAP M1 (US 915)',
-  gain: 1.2,
+  gain: 2.6,
 } as MakerAntenna
 
 const SENSECAP_EU = {
   name: 'SenseCAP M1 (EU 868)',
-  gain: 2.3,
+  gain: 2.8,
 } as MakerAntenna
 
 export default { SENSECAP_US, SENSECAP_EU }
