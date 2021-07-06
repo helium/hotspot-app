aa55fed76958eb06ed93803d4dc2aeccf1d17542
Matt Reetz <matthewcarlreetz@gmail.com>
2021-07-06 17:39:15 -0500

    Add qr strings example to maker files. (#759)

--- src/makers/__example__/hotspots.ts
+++ src/makers/__example__/hotspots.ts
@@ -41,7 +41,13 @@ const ExampleHotspotQR = {
   onboardType: 'QR',
   translations: {
     en: {
-      // TODO: Translate strings for diagnostic, power, and bluetooth
+      diagnostic:
+        '<b><white>Diagnostic support allows Example Maker to identify issues with your Hotspot in a secure way.</white></b>\n\nExample Maker will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>support@helium.com</b></purple> from the email used to purchase the Hotspot.',
+      power: [
+        'Attach the antenna and plug in the provided power adapter.',
+        'Your Hotspot will boot up, and its light will become Green when ready.',
+      ],
+      qr: 'Visit maker dashboard to generate a QR-code',
     },
     ja: {
       // TODO: Translate strings for diagnostic, power, and bluetooth
