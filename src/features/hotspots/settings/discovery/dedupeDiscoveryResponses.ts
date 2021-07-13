23c20595ea9cf3da44cc4d019bb19d20e786d682
Matt Reetz <matthewcarlreetz@gmail.com>
2021-07-13 15:57:52 -0500

    Filter disco responses with lodash/uniqBy

--- src/features/hotspots/settings/discovery/dedupeDiscoveryResponses.ts
+++ src/features/hotspots/settings/discovery/dedupeDiscoveryResponses.ts
@@ -1,9 +1,5 @@
+import { uniqBy } from 'lodash'
 import { DiscoveryResponse } from '../../../../store/discovery/discoveryTypes'
 
 export default (responses: DiscoveryResponse[]) =>
-  responses.filter(
-    (response, index, allResponses) =>
-      allResponses.findIndex(
-        (responseB) => responseB.hotspotAddress === response.hotspotAddress,
-      ) === index,
-  )
+  uniqBy(responses, ({ hotspotAddress }) => hotspotAddress)
