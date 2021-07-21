64081d16157ec166fc8283dca5e07ca78069473b
Liang Zhenwei <86951355+cotxliang@users.noreply.github.com>
2021-07-21 15:55:32 +0800

    Create hotspots.ts

---
+++ src/makers/cotx/hotspots.ts
@@ -0,0 +1,91 @@
+import HotspotIcon from './cotx.svg'
+import { MakerHotspot } from '../hotspotMakerTypes'
+import ANTENNAS from './antennas'
+
+const COTX_V1 = {
+  name: 'COTX Gateway',
+  onboardType: 'BLE',
+  translations: {
+    en: {
+      diagnostic:
+        '<b><white>Diagnostic support allows Helium to identify issues with your Hotspot in a secure way.</white></b>\n\nHelium will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>support@helium.com</b></purple> from the email used to purchase the Hotspot.',
+      power: [
+        'Plug in the provided power adapter into an outlet near a window.',
+        'The COTX Gateway Miner will show a red LED light once it’s powered on.',
+      ],
+      bluetooth: [
+        'There is no pairing button on the COTX Gateway Miner.',
+        'Bluetooth is automatically enabled for 5 minutes after the COTX Gateway Miner is powered on.\n\nHotspot can take up to 1 minute to fully boot up.',
+      ],
+    },
+    ja: {
+      diagnostic:
+        '<b><white>診断サポートにより、HeliumはHotspotの問題を安全な方法で特定できます。</white></b>\n\nHeliumが秘密キーにアクセスすることはありません。お使いのHotspotにのみアクセスし、ネットワーク上の他のデバイスにはアクセスしません。\n\n診断サポートをオプトアウトする場合は、Hotspotの購入時に使用したメールアドレスを使用して、<purple><b>support@helium.com</b></purple>までメールでご連絡ください。',
+      power: [
+        '付属の電源アダプターを窓の近くにあるコンセントに差し込みます。',
+        'COTX Gateway Minerの電源がオンになると、赤いLEDライトが点きます。',
+      ],
+      bluetooth: [
+        'COTX Gateway Minerにペアリングボタンはありません。',
+        'COTX Gateway Minerの電源がオンになると、自動的にBluetoothが5分間有効になります。\n\nHotspotが完全に起動するまでに最大で1分かかる場合があります。',
+      ],
+    },
+    ko: {
+      diagnostic:
+        '<b><white>Helium은 진단 지원을 통해 안전한 방법으로 Hotspot에서 발생하는 문제를 식별할 수 있습니다.</white></b>\n\nHelium은 개인 키에 대한 액세스 권한이 없으며 네트워크 내의 다른 기기를 제외하고 Hotspot에만 액세스할 수 있습니다.\n\n진단 지원을 선택 취소하려면 Hotspot을 구매할 때 사용한 이메일을 통해 <purple><b>support@helium.com</b></purple>으로 이메일을 보내주시기 바랍니다.',
+      power: [
+        '제공된 전원 어댑터를 창가 부근의 콘센트에 연결합니다.',
+        '전원이 켜지면 COTX Gateway Miner에 빨간색 LED 표시등이 표시됩니다.',
+      ],
+      bluetooth: [
+        'COTX Gateway Miner에는 페어링 버튼이 없습니다.',
+        'COTX Gateway Miner의 전원이 켜진 후 5분 동안 Bluetooth가 자동으로 활성화됩니다.\n\nHotspot이 완전히 부팅되는 데 최대 1분이 소요될 수 있습니다.',
+      ],
+    },
+    zh: {
+      diagnostic:
+        '<b><white>Hotspot 诊断支持帮助 Helium 安全确认您的 Hotspot 问题。</white></b>\n\nHelium 绝不会访问私人密钥，且仅可访问您的 Hotspot，无法访问您网络中的任何其他设备。\n\n若选择退出诊断支持，请使用购买 Hotspot 时提供的电子邮箱发送请求至 <purple><b>support@helium.com</b></purple>。',
+      power: [
+        '将电源适配器插入靠窗的插座。',
+        '开机后，COTX Gateway Miner 将亮起红色 LED 指示灯。',
+      ],
+      bluetooth: [
+        'COTX Gateway Miner 上没有配对按钮。',
+        'COTX Gateway Miner 开机后，蓝牙会自动启用 5 分钟。\n\nHotspot 最多需要 1 分钟即可完全启动。',
+      ],
+    },
+  },
+  icon: HotspotIcon,
+  antenna: { us: ANTENNAS.COTX_US, default: ANTENNAS.COTX_EU },
+} as MakerHotspot
+
+// const COTX_V2 = {
+//   name: 'COTX v2 Hotspot',
+//   onboardType:'QR',
+//   translations: {
+//     en: {
+//       diagnostic: 'Placeholder',
+//       power: ['Placeholder', 'Placeholder'],
+//       bluetooth: ['Placeholder', 'Placeholder'],
+//     },
+//     ko: {
+//       diagnostic: 'Placeholder',
+//       power: ['Placeholder', 'Placeholder'],
+//       bluetooth: ['Placeholder', 'Placeholder'],
+//     },
+//     ja: {
+//       diagnostic: 'Placeholder',
+//       power: ['Placeholder', 'Placeholder'],
+//       bluetooth: ['Placeholder', 'Placeholder'],
+//     },
+//     zh: {
+//       diagnostic: 'Placeholder',
+//       power: ['Placeholder', 'Placeholder'],
+//       bluetooth: ['Placeholder', 'Placeholder'],
+//     },
+//   },
+//   icon: HotspotIcon,
+//   antenna: { us: ANTENNAS.COTX_US, default: ANTENNAS.COTX_EU },
+// } as MakerHotspot
+
+export default { COTX_V1 /* , COTX_V2 */ }
