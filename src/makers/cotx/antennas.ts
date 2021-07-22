fb84171e37b347f83309f8c2ace34566a3b29070
Liang Zhenwei <86951355+cotxliang@users.noreply.github.com>
2021-07-22 14:08:05 +0800

    Update antennas.ts

--- src/makers/cotx/antennas.ts
+++ src/makers/cotx/antennas.ts
@@ -2,22 +2,17 @@ import { MakerAntenna } from '../antennaMakerTypes'
 
 const COTX_US = {
   name: 'COTX Gateway Miner (US 915)',
-  gain: 2.3,
+  gain: 3.6,
 } as MakerAntenna
 
 const COTX_EU = {
   name: 'COTX Gateway Miner (EU 868)',
-  gain: 2.8,
+  gain: 2.9,
 } as MakerAntenna
 
 const COTX_CN = {
   name: 'COTX Gateway Miner (CN 470)',
-  gain: 2.4,
+  gain: 2.8,
 } as MakerAntenna
 
-const COTX_CUSTOM = {
-  name: 'COTX Antenna',
-  gain: 5.8,
-} as MakerAntenna
-
-export default { COTX_US, COTX_EU, COTX_CN, COTX_CUSTOM }
+export default { COTX_US, COTX_EU, COTX_CN }
