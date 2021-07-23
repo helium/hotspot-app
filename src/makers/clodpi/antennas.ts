2f3364564e89b2d2f673a20ad039f515bc89a90d
KarthiG <karthi@clodpi.io>
2021-07-23 16:42:41 +0530

    Add ClodPi Hotspot

---
+++ src/makers/clodpi/antennas.ts
@@ -0,0 +1,8 @@
+import { MakerAntenna } from '../antennaMakerTypes'
+
+const CLODPI = {
+  name: 'ClodPi',
+  gain: 3,
+} as MakerAntenna
+
+export default { CLODPI }
--- src/makers/index.ts
+++ src/makers/index.ts
@@ -1,4 +1,5 @@
 import bobcat from './bobcat'
+import clodpi from './clodpi'
 import customAntennas from './custom/antennas'
 import finestra from './finestra'
 import helium from './helium'
@@ -13,6 +14,7 @@ import { HotspotMakerLangField } from './hotspotMakerTypes'
 
 export const Makers: Record<string, { id: number; supportEmail: string }> = {
   bobcat,
+  clodpi,
   finestra,
   helium,
   longAP,
@@ -25,6 +27,7 @@ export const Makers: Record<string, { id: number; supportEmail: string }> = {
 
 export const AntennaModels = {
   ...bobcat.antennas,
+  ...clodpi.antennas,
   ...customAntennas,
   ...finestra.antennas,
   ...helium.antennas,
@@ -38,6 +41,7 @@ export const AntennaModels = {
 
 export const HotspotMakerModels = {
   ...bobcat.hotspots,
+  ...clodpi.hotspots,
   ...finestra.hotspots,
   ...helium.hotspots,
   ...longAP.hotspots,
