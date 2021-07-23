cf6a233fa0e0f82bac93a3d190e5dff2cc6b3e13
Heltec-Aaron-Lee <support@heltec.cn>
2021-07-24 01:30:04 +0800

    Update maker name.

--- src/makers/heltec/hotspots.ts
+++ src/makers/heltec/hotspots.ts
@@ -9,7 +9,7 @@ const HELTEC_INDOOR = {
   translations: {
     en: {
       diagnostic:
-        '<b><white>Diagnostic support allows Example Maker to identify issues with your Hotspot in a secure way.</white></b>\n\nHeltec will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>support@heltec.cn</b></purple> from the email used to purchase the Hotspot.',
+        '<b><white>Diagnostic support allows Heltec Automation to identify issues with your Hotspot in a secure way.</white></b>\n\nHeltec will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>support@heltec.cn</b></purple> from the email used to purchase the Hotspot.',
       power: [
         'Attach the antenna and plug in the provided power adapter',
         'Once powered on, the Heltec Indoor Hotspot will light up several indicators one after another. \n\nStartup takes about 30 seconds, and when the yellow indicator is on or blinking, the system is completely ready to work.',
@@ -45,7 +45,7 @@ const HELTEC_INDOOR = {
     },
     zh: {
       diagnostic:
-        '<b><white>Hotspot 诊断支持帮助 Heltec 安全确认您的 Hotspot 问题。</white></b>\n\nHeltec 绝不会访问私人密钥，且仅可访问您的 Hotspot，无法访问您网络中的任何其他设备。\n\n若选择退出诊断支持，请使用购买 Hotspot 时提供的电子邮箱发送请求至 <purple><b>support@heltec.cn</b></purple>。',
+        '<b><white>Hotspot 诊断支持允许 Heltec Automation 安全确认您的 Hotspot 问题。</white></b>\n\nHeltec 绝不会访问私人密钥，且仅可访问您的 Hotspot，无法访问您网络中的任何其他设备。\n\n若选择退出诊断支持，请使用购买 Hotspot 时提供的电子邮箱发送请求至 <purple><b>support@heltec.cn</b></purple>。',
       power: [
         '连接天线并正确连接电源。',
         '开机后, Heltec Indoor Hotspot 将陆续亮起多个指示灯。\n\n启动需要大约30秒，当黄灯亮起或者闪烁时，系统启动完成。',
