8ab73be027cd8ed35601b697f51f9ec41ffec93a
Tim Cooijmans <timcooijmans@gmail.com>
2021-07-29 13:59:58 +0200

    Update LongAP Pro resources and add LongAP Light

--- src/makers/longAP/antennas.ts
+++ src/makers/longAP/antennas.ts
@@ -1,8 +1,23 @@
 import { MakerAntenna } from '../antennaMakerTypes'
 
 const LONG_AP_ONE_EU = {
-  name: 'LongAP One (EU 868)',
+  name: 'LongAP One',
   gain: 3,
 } as MakerAntenna
 
-export default { LONG_AP_ONE_EU }
+const LONG_AP_LIGHT = {
+  name: 'LongAP Light',
+  gain: 3,
+} as MakerAntenna
+
+const LONG_AP_PRO_3 = {
+  name: 'LongAP One',
+  gain: 3,
+} as MakerAntenna
+
+const LONG_AP_PRO_6 = {
+  name: 'LongAP One',
+  gain: 6,
+} as MakerAntenna
+
+export default { LONG_AP_ONE_EU, LONG_AP_LIGHT, LONG_AP_PRO_3, LONG_AP_PRO_6 }
