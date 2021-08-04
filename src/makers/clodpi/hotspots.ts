5ff8d084c60b3846bf7c4eb9b1d03e61037e591e
Karthi G <47271459+karthiclodpi@users.noreply.github.com>
2021-08-04 23:02:35 +0530

    Update hotspots.ts

--- src/makers/clodpi/hotspots.ts
+++ src/makers/clodpi/hotspots.ts
@@ -10,8 +10,7 @@ const ClodPi = {
   translations: {
     en: {
       diagnostic:
-                '<b><white>Diagnostic support allows ClodPi to identify issues with your Hotspot in a secure way.</white></b>\nClodPi will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>support@clodpi.io</b></purple> from the email used to purchase the Hotspot.',
-
+        '<b><white>Diagnostic support allows ClodPi to identify issues with your Hotspot in a secure way.</white></b>\nClodPi will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>support@clodpi.io</b></purple> from the email used to purchase the Hotspot.',
       power: [
         'Attach the antenna and plug in the provided power adapter.',
         'The ClodPi Pro PWR LED will light up once it’s powered on.',
