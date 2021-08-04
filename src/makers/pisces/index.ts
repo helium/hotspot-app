75b60653c863779d98b59d94f64d935424b0da8e
pisces <83175310+piscesminer@users.noreply.github.com>
2021-08-05 01:40:56 +0800

    Update makerId
    Update makerId

--- src/makers/pisces/index.ts
+++ src/makers/pisces/index.ts
@@ -4,6 +4,6 @@ import hotspots from './hotspots'
 export default {
   antennas,
   hotspots,
-  id: 1616, // TODO: Waiting for maker id from onboarding server
+  id: 13, // Our maker id from onboarding server
   supportEmail: 'admin@piscesminer.com',
 }
