136fd91653b0bf56ccfb9ff6ee928a43d0255bd4
controllinohelium <86105933+controllinohelium@users.noreply.github.com>
2021-08-31 13:43:57 +0200

    Update index.ts

--- src/makers/controllino/index.ts
+++ src/makers/controllino/index.ts
@@ -4,6 +4,6 @@ import hotspots from './hotspots'
 export default {
   antennas,
   hotspots,
-  id: 99999, // Waiting for DeWi to provide us maker id
-  supportEmail: 'hotspotsupport@controllino.com',
+  id: 17,
+  supportEmail: 'helium@controllino.com',
 }
