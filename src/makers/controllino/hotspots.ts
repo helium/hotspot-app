22dd6b106f428f3055514ee1b512b1c178ad1fb6
controllinohelium <86105933+controllinohelium@users.noreply.github.com>
2021-09-03 09:56:33 +0200

    Update hotspots.ts
    Removed chinese translations as they caused some tslint issues

--- src/makers/controllino/hotspots.ts
+++ src/makers/controllino/hotspots.ts
@@ -19,18 +19,6 @@ const ControllinoHotspot = {
         'If you cannot see the Hotspot after a few minutes, unplug the power cable and plug it in again.',
       ],
     },
-    zh: {
-		diagnostic:
-			'<b><white>诊断支持允许 Controllino 以安全的方式识别热点问题</white></b>\n\nControllino 永远无法访问私钥，并且只能访问您的热点，而不能访问您网络上的任何其他设备。\n\n如果您想选择退出诊断支持，请通过用于购买 Hotspot 的电子邮件发送电子邮件至 <purple><b>hotspotsupport@controllino.com</b></purple>',
-		power: [
-		'按照用户手册中的说明连接天线并插入电源线',
-		'您的热点将在大约 5 分钟内启动并准备就绪',
-		],
-		bluetooth: [
-		'插入热点 5 分钟后，您应该会在手机上看到一个蓝牙设备',
-		'如果几分钟后看不到热点，请拔下电源线并重新插入',
-		],
-    },
   },
   antenna: {
     default: ANTENNAS.CONTROLLINO_EU,
