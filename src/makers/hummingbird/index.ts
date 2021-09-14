16f1efc544c63d5ff6fe4ff817d29e014216adec
yango3 <o3.yang@hotmail.com>
2021-09-14 12:01:03 +0800

    add hummingbird

---
+++ src/makers/hummingbird/index.ts
@@ -0,0 +1,9 @@
+import antennas from './antennas'
+import hotspots from './hotspots'
+
+export default {
+  antennas,
+  hotspots,
+  id: 66, // TODO: update when maker ID is assigned
+  supportEmail: 'hummingbirdminer@outlook.com',
+}
