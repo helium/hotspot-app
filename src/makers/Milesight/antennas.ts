70972c1c408d6793a3f81bfda36bac98ca254414
Milesight IoT <75722092+Milesight-IoT@users.noreply.github.com>
2021-09-22 11:02:28 +0800

    Add Milesight hotspot

---
+++ src/makers/Milesight/antennas.ts
@@ -0,0 +1,28 @@
+import { MakerAntenna } from '../antennaMakerTypes'
+
+const MILESIGHT_UG65 = {
+  name: 'Milesight Hotspot UG65',
+  gain: 1,
+} as MakerAntenna
+
+const MILESIGHT_UG67 = {
+  name: 'Milesight Hotspot UG67',
+  gain: 5,
+} as MakerAntenna
+
+const Custom7dBi = {
+  name: 'Custom 7dBi',
+  gain: 7,
+} as MakerAntenna
+
+const Custom8dBi = {
+  name: 'Custom 8dBi',
+  gain: 8,
+} as MakerAntenna
+
+export default {
+  MILESIGHT_UG65,
+  MILESIGHT_UG67,
+  Custom7dBi,
+  Custom8dBi,
+}
