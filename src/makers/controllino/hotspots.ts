812ebec0a0d776854ded6cf161028d0677d00e9a
controllinohelium <86105933+controllinohelium@users.noreply.github.com>
2021-10-22 15:05:12 +0200

    Updated text to reflect onboarding changes

--- src/makers/controllino/hotspots.ts
+++ src/makers/controllino/hotspots.ts
@@ -12,11 +12,11 @@ const ControllinoHotspot = {
         '<b><white>Diagnostic support allows Controllino to identify issues with your Hotspot in a secure way.</white></b>\n\nControllino will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>hotspotsupport@controllino.com</b></purple> from the email used to purchase the Hotspot.',
       power: [
         'Attach the antenna and plug in the power cable as described in the user manual.',
-        'Your Hotspot will boot up and is  ready in around 5 minutes.',
+        'Your Hotspot will boot up. The blue LED will blink once for 1 second when done.',
       ],
       bluetooth: [
-        'After roughly 5 minutes after plugging in the Hotspot, you should see it as Bluetooth device on your phone.',
-        'If you cannot see the Hotspot after a few minutes, unplug the power cable and plug it in again.',
+        'After you see the blue LED light up 3 TIMES, press the button below to start pairing.',
+        'If you cannot see it in the app, try scanning again. If it fails multiple times, unplug the power cable and plug it in to start the process again.',
       ],
     },
   },
