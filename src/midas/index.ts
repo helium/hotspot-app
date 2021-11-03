f8bbc0c5c9b23d6526b2ca0d414250d5339cde80
MidasWireless <86901323+MidasWireless@users.noreply.github.com>
2021-11-03 11:14:41 +0800

    Create index.ts

---
+++ src/midas/index.ts
@@ -0,0 +1,9 @@
+import antennas from './antennas'
+import hotspots from './hotspots'
+
+export default {
+  antennas,
+  hotspots,
+  id: 20, // TODO: Get maker id from onboarding server
+  supportEmail: 'support@midaswireless.com',
+}
