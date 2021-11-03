d9b35f871e30040a00a2834fb7aec2eda8244379
MidasWireless <86901323+MidasWireless@users.noreply.github.com>
2021-11-03 11:43:05 +0800

    Update antennas.ts

--- src/makers/midas/antennas.ts
+++ src/makers/midas/antennas.ts
@@ -2,12 +2,17 @@ import { MakerAntenna } from '../antennaMakerTypes'
 
 const MIDAS_GATEWAY_US = {
   name: 'MIDAS_GATEWAY(US 915)',
-  gain: 1.2,
+  gain: 3,
 } as MakerAntenna
 
 const MIDAS_GATEWAY_EU = {
   name: 'MIDAS_GATEWAY (EU 868)',
-  gain: 2.3,
+  gain: 3,
 } as MakerAntenna
 
-export default { MIDAS_GATEWAY_US, MIDAS_GATEWAY_EU }
+const MIDAS_GATEWAY_CN = {
+  name: 'MIDAS_GATEWAY (CN 470)',
+  gain: 3,
+} as MakerAntenna
+
+export default { MIDAS_GATEWAY_US, MIDAS_GATEWAY_EU, MIDAS_GATEWAY_CN }
