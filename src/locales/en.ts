fc7d696dfcf0747e0463912a9e5b17790f4bc513
Tyler Whitman <twhit093@gmail.com>
2021-11-05 10:02:33 -0700

    remove translations

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -442,12 +442,6 @@ export default {
     },
   },
   wallet: {
-    empty: {
-      title: 'Welcome to\nyour wallet',
-      subtitle: 'Your balance is zero.',
-      description:
-        'You can send HNT to the below address/QR or deploy a Hotspot to start earning.',
-    },
     title: 'My Wallet',
     copiedToClipboard: 'Copied {{address}} to clipboard',
     share: 'Share',
