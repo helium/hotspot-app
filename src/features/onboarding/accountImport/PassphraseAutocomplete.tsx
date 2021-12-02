78fd06c608353544ed35a3c33eae1665d3ecd288
Tyler Whitman <twhit093@gmail.com>
2021-12-02 14:48:14 -0800

    fix lint error

--- src/features/onboarding/accountImport/PassphraseAutocomplete.tsx
+++ src/features/onboarding/accountImport/PassphraseAutocomplete.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import {
   Keyboard,
   KeyboardAvoidingView,
