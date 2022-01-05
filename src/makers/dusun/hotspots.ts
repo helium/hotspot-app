63c96f9444894dc7a7e8a76172ae312d42350e37
pwj <panwenjian@yeah.net>
2022-01-05 14:39:34 +0800

    Add Chinese language

--- src/makers/dusun/hotspots.ts
+++ src/makers/dusun/hotspots.ts
@@ -26,7 +26,16 @@ const DusunHotspotBLE = {
       // TODO: Translate strings for diagnostic, power, and bluetooth
     },
     zh: {
-      // TODO: Translate strings for diagnostic, power, and bluetooth
+      diagnostic:
+        '<b><white>诊断支持帮助 Dusun 安全确认您的 Hotspot 问题。</white></b>\n\nDusun 绝不会访问私人密钥，且仅可访问您的 Hotspot，不会访问您网络中的任何其他设备。\n\n若选择退出诊断支持，请使用购买 Hotspot 时提供的电子邮箱发送请求至 <purple><b>support@hzdusun.com</b></purple>。',
+      power: [
+        '连接天线，将随附的USB电源适配器插入靠窗的插座。',
+        '开机后，Hotspot 将亮红色 LED 指示灯，启动好并连上网络后将变蓝色。',
+      ],
+      bluetooth: [
+        '按一下 Hotspot 侧面的凸出按钮。',
+        'LED 指示灯将变成紫色闪烁，即表示 Hotspot 已准备进行配对。\n\n按“下一步”开始扫描。',
+      ],
     },
   },
   antenna: {
