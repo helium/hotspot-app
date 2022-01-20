6819cefeb18ff15fe8aeded15f55ad87a768b723
ninajocson <loudadimn@gmail.com>
2022-01-20 17:37:40 +0500

    Update antennas.ts

--- src/makers/heliumrepublic/antennas.ts
+++ src/makers/heliumrepublic/antennas.ts
@@ -1,8 +1,28 @@
 import { MakerAntenna } from '../antennaMakerTypes'
 
-const heliumrepublic = {
+const heliumrepublic_3 = {
   name: 'HELIUM REPUBLIC',
   gain: 3,
 } as MakerAntenna
 
-export default { heliumrepublic }
+const heliumrepublic_4 = {
+  name: 'HELIUM REPUBLIC',
+  gain: 3,
+} as MakerAntenna
+
+const heliumrepublic_5 = {
+  name: 'HELIUM REPUBLIC',
+  gain: 3,
+} as MakerAntenna
+const heliumrepublic_8 = {
+  name: 'HELIUM REPUBLIC',
+  gain: 3,
+} as MakerAntenna
+
+
+export default { 
+  heliumrepublic_3,
+  heliumrepublic_4,
+  heliumrepublic_5,
+  heliumrepublic_8
+}
