97bcd121223dc0d0d5dd88cf8219fb759705835e
ninajocson <loudadimn@gmail.com>
2022-01-20 17:48:17 +0500

    Update antennas.ts

--- src/makers/heliumrepublic/antennas.ts
+++ src/makers/heliumrepublic/antennas.ts
@@ -7,16 +7,23 @@ const heliumrepublic_3 = {
 
 const heliumrepublic_4 = {
   name: 'HELIUM REPUBLIC',
-  gain: 3,
+  gain: 4,
 } as MakerAntenna
 
 const heliumrepublic_5 = {
   name: 'HELIUM REPUBLIC',
-  gain: 3,
+  gain: 5,
 } as MakerAntenna
+
+const heliumrepublic_58 = {
+  name: 'HELIUM REPUBLIC',
+  gain: 5.8,
+} as MakerAntenna
+
+
 const heliumrepublic_8 = {
   name: 'HELIUM REPUBLIC',
-  gain: 3,
+  gain: 8,
 } as MakerAntenna
 
 
@@ -24,5 +31,6 @@ export default {
   heliumrepublic_3,
   heliumrepublic_4,
   heliumrepublic_5,
+  heliumrepublic_58,
   heliumrepublic_8
 }
