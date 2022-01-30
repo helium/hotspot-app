9cd6e177eaff5e06d1207a5d5540dacbbf9e1361
Joycewoogator <joyce@gatorcn.com>
2022-01-30 17:46:43 +0800

    my files

---
+++ src/makers/gatorpro/antennas.ts
@@ -0,0 +1,13 @@
+import { MakerAntenna } from '../antennaMakerTypes'
+
+const EXAMPLE_US = {
+  name: 'Example Hotspot (US 915)',
+  gain: 1.2,
+} as MakerAntenna
+
+const EXAMPLE_EU = {
+  name: 'Example Hotspot (EU 868)',
+  gain: 2.3,
+} as MakerAntenna
+
+export default { EXAMPLE_US, EXAMPLE_EU }
