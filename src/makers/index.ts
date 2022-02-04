90c29e8b83f51ddda9ce48cef78931ef4b41f12f
edwin <edwin.voip@gmail.com>
2022-02-05 03:38:13 +0800

    Add Dragino HP0D (#1229)

--- src/makers/index.ts
+++ src/makers/index.ts
@@ -22,6 +22,7 @@ import merryIoT from './merryIoT'
 import milesight from './milesight'
 import midas from './midas'
 import deepernetwork from './deepernetwork'
+import dragino from './dragino'
 import { LangType, supportedLangs } from '../utils/i18n/i18nTypes'
 import { HotspotMakerLangField } from './hotspotMakerTypes'
 
@@ -49,6 +50,7 @@ export const Makers: Record<string, { id: number; supportEmail: string }> = {
   milesight,
   midas,
   deepernetwork,
+  dragino,
 }
 
 export const AntennaModels = {
@@ -76,6 +78,7 @@ export const AntennaModels = {
   ...milesight.antennas,
   ...midas.antennas,
   ...deepernetwork.antennas,
+  ...dragino.antennas,
 }
 
 export const HotspotMakerModels = {
@@ -102,6 +105,7 @@ export const HotspotMakerModels = {
   ...milesight.hotspots,
   ...midas.hotspots,
   ...deepernetwork.hotspots,
+  ...dragino.hotspots,
 }
 
 export type HotspotType = keyof typeof HotspotMakerModels
