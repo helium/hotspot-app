139b88627aa17602ab314b930a70b8fe80ac507a
Coco Tang <coco@helium.com>
2022-02-22 14:56:51 -0800

    Update hotspots.ts

--- src/makers/rak/hotspots.ts
+++ src/makers/rak/hotspots.ts
@@ -8,7 +8,7 @@ const RAK_V1 = {
   translations: {
     en: {
       diagnostic:
-        '<b><white>Diagnostic support allows Helium to identify issues with your Hotspot in a secure way.</white></b>\n\nHelium will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>support@helium.com</b></purple> from the email used to purchase the Hotspot.',
+        '<b><white>Diagnostic support allows MNTD to identify issues with your Hotspot in a secure way.</white></b>\n\MNTD will never have access to private keys and will only ever be able to access your Hotspot and not any other devices on your Network.\n\nIf you would like to opt-out of diagnostic support please email <purple><b>app@send.getmntd.com</b></purple> from the email used to purchase the Hotspot.',
       power: [
         'Plug in the provided power adapter into an outlet near a window.',
         'The RAK Hotspot Miner will show a red LED light once it’s powered on.',
@@ -20,7 +20,7 @@ const RAK_V1 = {
     },
     ja: {
       diagnostic:
-        '<b><white>診断サポートにより、HeliumはHotspotの問題を安全な方法で特定できます。</white></b>\n\nHeliumが秘密キーにアクセスすることはありません。お使いのHotspotにのみアクセスし、ネットワーク上の他のデバイスにはアクセスしません。\n\n診断サポートをオプトアウトする場合は、Hotspotの購入時に使用したメールアドレスを使用して、<purple><b>support@helium.com</b></purple>までメールでご連絡ください。',
+        '<b><white>診断サポートにより、MNTDはHotspotの問題を安全な方法で特定できます。</white></b>\n\nMNTDが秘密キーにアクセスすることはありません。お使いのHotspotにのみアクセスし、ネットワーク上の他のデバイスにはアクセスしません。\n\n診断サポートをオプトアウトする場合は、Hotspotの購入時に使用したメールアドレスを使用して、<purple><b>app@send.getmntd.com</b></purple>までメールでご連絡ください。',
       power: [
         '付属の電源アダプターを窓の近くにあるコンセントに差し込みます。',
         'RAK Hotspot Minerの電源がオンになると、赤いLEDライトが点きます。',
@@ -32,7 +32,7 @@ const RAK_V1 = {
     },
     ko: {
       diagnostic:
-        '<b><white>Helium은 진단 지원을 통해 안전한 방법으로 Hotspot에서 발생하는 문제를 식별할 수 있습니다.</white></b>\n\nHelium은 개인 키에 대한 액세스 권한이 없으며 네트워크 내의 다른 기기를 제외하고 Hotspot에만 액세스할 수 있습니다.\n\n진단 지원을 선택 취소하려면 Hotspot을 구매할 때 사용한 이메일을 통해 <purple><b>support@helium.com</b></purple>으로 이메일을 보내주시기 바랍니다.',
+        '<b><white>MNTD은 진단 지원을 통해 안전한 방법으로 Hotspot에서 발생하는 문제를 식별할 수 있습니다.</white></b>\n\nMNTD은 개인 키에 대한 액세스 권한이 없으며 네트워크 내의 다른 기기를 제외하고 Hotspot에만 액세스할 수 있습니다.\n\n진단 지원을 선택 취소하려면 Hotspot을 구매할 때 사용한 이메일을 통해 <purple><b>app@send.getmntd.com</b></purple>으로 이메일을 보내주시기 바랍니다.',
       power: [
         '제공된 전원 어댑터를 창가 부근의 콘센트에 연결합니다.',
         '전원이 켜지면 RAK Hotspot Miner에 빨간색 LED 표시등이 표시됩니다.',
@@ -44,7 +44,7 @@ const RAK_V1 = {
     },
     zh: {
       diagnostic:
-        '<b><white>Hotspot 诊断支持帮助 Helium 安全确认您的 Hotspot 问题。</white></b>\n\nHelium 绝不会访问私人密钥，且仅可访问您的 Hotspot，无法访问您网络中的任何其他设备。\n\n若选择退出诊断支持，请使用购买 Hotspot 时提供的电子邮箱发送请求至 <purple><b>support@helium.com</b></purple>。',
+        '<b><white>Hotspot 诊断支持帮助 MNTD 安全确认您的 Hotspot 问题。</white></b>\n\nMNTD 绝不会访问私人密钥，且仅可访问您的 Hotspot，无法访问您网络中的任何其他设备。\n\n若选择退出诊断支持，请使用购买 Hotspot 时提供的电子邮箱发送请求至 <purple><b>app@send.getmntd.com</b></purple>。',
       power: [
         '将电源适配器插入靠窗的插座。',
         '开机后，RAK Hotspot Miner 将亮起红色 LED 指示灯。',
