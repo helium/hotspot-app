1f47940a59664bf3721e22de3cc04f9e9ce7ab56
Tyler Whitman <twhit093@gmail.com>
2022-05-13 11:40:45 -0700

    remove relay article

--- src/constants/articles.ts
+++ src/constants/articles.ts
@@ -1,5 +1,4 @@
 enum Articles {
-  Relay = 'https://docs.helium.com/troubleshooting/network-troubleshooting',
   Token_Earnings = 'https://docs.helium.com/blockchain/mining/#how-do-hotspots-earn-helium-tokens',
   Helium_Token = 'https://docs.helium.com/blockchain/helium-token',
   Docs_Root = 'https://docs.helium.com',
