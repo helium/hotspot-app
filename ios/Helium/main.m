b6403da46716b492385e64d1c02d68a73bdca797
Tyler Whitman <twhit093@gmail.com>
2022-12-15 15:58:22 -0800

    upgrade to RN 70

--- ios/Helium/main.m
+++ ios/Helium/main.m
@@ -2,7 +2,8 @@
 #import "Helium-Swift.h"
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[])
+{
   @autoreleasepool {
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
